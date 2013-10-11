b63cd54f5082292bfe363b593074f8781255c8d1
mod <muzcads@gmail.com>
2013-10-11 10:57:20 +0700

    Update div region to contain search result via ajax call

--- application/views/home.php
+++ application/views/home.php
@@ -80,19 +80,20 @@
           <div class="row">
             <div class="col-lg-12">
               <h2>1,298 Conferences found</h2> <br>
-            <div class="row conf-list">
+            <div class="row">
 	            <div class="col-lg-12">
-
+                <div id="conf-list">
 	                <?php foreach ($conferences as $conf): ?>
 	                	<div class="row">
-		              	<div class="col-lg-6">
-		              	  <p><strong><a href="schedule.html"><?php echo $conf['name'] ?></a></strong> <br/> <?php echo $conf['location'] ?></p>
-		              	</div>
-		              	<div class="col-lg-2"><?php echo $conf['type'] ?></div>
-		              	<div class="col-lg-2"><?php echo $conf['duration'] ?></div>
-		              	<div class="col-lg-2"><a class="btn btn-info" href="#">Book</a></div>
-		              </div><!--/row-->
-	            	<?php endforeach ?>
+  		              	<div class="col-lg-6">
+  		              	  <p><strong><a href="schedule.html"><?php echo $conf['name'] ?></a></strong> <br/> <?php echo $conf['location'] ?></p>
+  		              	</div>
+  		              	<div class="col-lg-2"><?php echo $conf['type'] ?></div>
+  		              	<div class="col-lg-2"><?php echo $conf['duration'] ?></div>
+  		              	<div class="col-lg-2"><a class="btn btn-info" href="#">Book</a></div>
+		                </div><!--/row-->
+	            	  <?php endforeach ?>
+                </div>
 	              
 	              <div class="row">
 	              	<div class="col-lg-12">
