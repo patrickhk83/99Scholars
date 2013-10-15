1a4304fad63a1b9eebc2bbbc701edb7babd1d825
mod <muzcads@gmail.com>
2013-10-15 17:03:21 +0700

    Add conference link in home page

--- application/views/home.php
+++ application/views/home.php
@@ -91,7 +91,7 @@
 	                <?php foreach ($conferences as $conf): ?>
 	                	<div class="row">
   		              	<div class="col-lg-6">
-  		              	  <p><strong><a href="schedule.html"><?php echo $conf['name'] ?></a></strong> <br/> <?php echo $conf['location'] ?></p>
+  		              	  <p><strong><a href="<?php echo URL::site('conference/view/'.$conf['id'])?>"><?php echo $conf['name'] ?></a></strong> <br/> <?php echo $conf['location'] ?></p>
   		              	</div>
   		              	<div class="col-lg-2"><?php echo $conf['type'] ?></div>
   		              	<div class="col-lg-2"><?php echo $conf['duration'] ?></div>
