738855a2287488b0a60235424afaf345a27efd7a
mod <muzcads@gmail.com>
2013-10-26 16:02:08 +0700

    Add Profile controller

---
+++ application/classes/Controller/Profile.php
@@ -0,0 +1,19 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Profile extends Controller {
+
+	public function action_index()
+	{
+		$login_service = new Service_Login();
+
+		if(!$login_service->is_login())
+		{
+			$this->redirect('/', 302);
+		}
+		else
+		{
+			//TODO: get current user
+			
+		}
+	}
+}
