dab3e3130fc3496f365dfdb14954d3446e005a18
mod <muzcads@gmail.com>
2013-10-28 13:17:54 +0700

    Add login page

--- application/classes/Controller/Login.php
+++ application/classes/Controller/Login.php
@@ -4,20 +4,34 @@ class Controller_Login extends Controller {
 
 	public function action_index()
 	{
-		$login_service = new Service_Login();
-
-		$email = $this->request->post('email');
-		$password = $this->request->post('password');
-		
-
-		if($login_service->login($email, $password))
+		if(HTTP_Request::POST == $this->request->method())
 		{
-			$this->redirect('/profile', 302);
+			$login_service = new Service_Login();
+
+			$email = $this->request->post('email');
+			$password = $this->request->post('password');
+			
+
+			if($login_service->login($email, $password))
+			{
+				$this->redirect('/profile', 302);
+			}
+			else
+			{
+				$view = View::factory('login');
+
+				//TODO: add type of error
+				$view->error = "Can not sign in to 99Scholars. Please check your email and password";
+				$this->response->body($view);
+			}
 		}
 		else
 		{
-			echo "fail";
+			$view = View::factory('login');
+			$this->response->body($view);
 		}
+
+		
 		
 	}
 }
