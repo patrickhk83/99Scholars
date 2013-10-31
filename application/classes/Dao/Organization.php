8fde1cb3e3ab8dbd16739f90871ba1ea8b5ecbc0
mod <muzcads@gmail.com>
2013-10-31 11:57:14 +0700

    Create Organization DAO

---
+++ application/classes/Dao/Organization.php
@@ -0,0 +1,16 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Dao_Organization {
+
+	public function create($name, $description)
+	{
+		$org = ORM::factory('Organization');
+
+		$org->name = $name;
+		$org->description = $description;
+
+		$org->save();
+
+		return $org->pk();
+	}
+}
