f7590bcafaca7a16d5c0292cdece76730bbaa737
mod <muzcads@gmail.com>
2013-11-04 21:43:03 +0700

    Refactor javascript in seminar page

--- js/seminar.js
+++ js/seminar.js
@@ -1,22 +1,34 @@
+var baseUrl = '';
+var moduleName = 'conference';
+var baseConfUrl = '';
+
 $(function(){
+
+	baseUrl = $('#base-url').val();
+	baseConfUrl = baseUrl + moduleName + "/";
+
 	$('#attachment-tab a:first').tab('show');
 	
-	$('#join-btn').click(function () {
-		alert('Thanks for joining this seminar');
-		
-		var html = '<tr style="display:none">' + 
-			'<td width="40px"><img src="img/avatar.jpg" width="40"/></td>' +
-		  '<td>' +
-		    '<p><a href="#"><strong>John Doe</strong></a> <br/> <small class="text-muted">Massachusetts Institute of Technology</small></p>' +
-		    '<p></p>' +
-		  '</td>' +
-		'</tr>';
-		
-		var ele = $(html);
-		
-		$('#attendee-list').append(ele);
-		ele.show('slow');
-		
-		return false;
-	});
+	$('#join-btn').click(joinSeminar);
 });
+
+var joinSeminar = function()
+{
+
+	alert('Thanks for joining this seminar');
+		
+	var html = '<tr style="display:none">' + 
+		'<td width="40px"><img src="img/avatar.jpg" width="40"/></td>' +
+	  '<td>' +
+	    '<p><a href="#"><strong>John Doe</strong></a> <br/> <small class="text-muted">Massachusetts Institute of Technology</small></p>' +
+	    '<p></p>' +
+	  '</td>' +
+	'</tr>';
+	
+	var ele = $(html);
+	
+	$('#attendee-list').append(ele);
+	ele.show('slow');
+	
+	return false;
+}
