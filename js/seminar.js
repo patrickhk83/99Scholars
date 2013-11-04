2045e4b1df363967fe1c708f47b014a15b13ec0c
mod <muzcads@gmail.com>
2013-11-04 21:51:02 +0700

    Add attend function in javascript

--- js/seminar.js
+++ js/seminar.js
@@ -14,21 +14,27 @@ $(function(){
 
 var joinSeminar = function()
 {
+	var confId = $('#conf-id').val();
 
-	alert('Thanks for joining this seminar');
+	var url = baseConfUrl + 'attend/' + confId;
+
+	$.get(url, function(data){
+
+		alert('Thanks for joining this seminar');
 		
-	var html = '<tr style="display:none">' + 
-		'<td width="40px"><img src="img/avatar.jpg" width="40"/></td>' +
-	  '<td>' +
-	    '<p><a href="#"><strong>John Doe</strong></a> <br/> <small class="text-muted">Massachusetts Institute of Technology</small></p>' +
-	    '<p></p>' +
-	  '</td>' +
-	'</tr>';
-	
-	var ele = $(html);
-	
-	$('#attendee-list').append(ele);
-	ele.show('slow');
+		var html = '<tr style="display:none">' + 
+			'<td width="40px"><img src="img/avatar.jpg" width="40"/></td>' +
+		  '<td>' +
+		    '<p><a href="#"><strong>John Doe</strong></a> <br/> <small class="text-muted">Massachusetts Institute of Technology</small></p>' +
+		    '<p></p>' +
+		  '</td>' +
+		'</tr>';
+		
+		var ele = $(html);
+		
+		$('#attendee-list').append(ele);
+		ele.show('slow');
+	});
 	
 	return false;
 }
