c3f4ec9cd6a8c93628d5f910644a374842937a6a
mod <muzcads@gmail.com>
2013-11-13 14:25:54 +0700

    Fix user profile URL

--- application/views/discussion/topic_title.php
+++ application/views/discussion/topic_title.php
@@ -2,7 +2,7 @@
   <td>
     <p>
       <strong><a href="#" class="topic-title" id="topic-<?php echo $topic['id'] ?>"><?php echo $topic['title'] ?></a></strong> 
-      <br><small><a href="<?php echo URL::site('user/view/'.$topic['author_id']) ?>"><?php echo $topic['author_name'] ?></a> <span class="text-muted"><?php echo $topic['last_update'] ?></span></small>
+      <br><small><a href="<?php echo URL::site('user/profile/'.$topic['author_id']) ?>"><?php echo $topic['author_name'] ?></a> <span class="text-muted"><?php echo $topic['last_update'] ?></span></small>
     </p>
   </td>
 </tr>
