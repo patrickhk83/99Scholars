9f95aa64615bd47afeb50f4001206314c5a652c6
kahihia <kajoengasa@yahoo.com>
2013-12-08 19:41:58 +0200

    blog css

--- application/views/article/index.php
+++ application/views/article/index.php
@@ -1,24 +1,31 @@
 <?php defined('SYSPATH') or die('No direct script access.'); ?>
-    <div id="content">  <div class="blog clearfix">
-        <div id="left-side" class="float-left" style="float:left">
+ <div id="content">   <h2><span>Blog</span></h2>
+    <div class="blog clearfix" >
+        <div id="left-side" class="float-left" >
 		
 		  <?php foreach($art as $article): ?> 
           <div class="blog-post">
-            <div class="blog-title clearfix"><a><?php echo HTML::anchor("article/view/".$article->id, $article->title); ?><p class="float-right">Posted by: <span>Admin</span></p></div>
-           
+ 
+            <div class="blog-title clearfix"><a href="blog-post.html"><?php echo HTML::anchor("article/view/".$article->id, $article->article_title); ?></a><p class="float-right">Posted by: <span>Admin</span></p></div>
+            <div class="blog-prew">
+              <iframe src="<?php echo $article->image; ?>" width="666" height="285"></iframe>
+            </div>
             <div class="blog-prew-shadow"><!-- --></div>
             <div class="blog-options clearfix">
-              <div class="b-posted float-left">Posted on: <span><?php echo $article->time; ?> </span></div>
-              <div class="b-category float-left">Category: <a href="#"><?php echo HTML::anchor("article/edit/".$article->id, "Edit"); ?></a></div>
-              <div class="b-comments float-left"><?php echo HTML::anchor("article/delete/".$article->id, "Delete"); ?></div>
-              <div class="b-views float-left"><span>211 views</span></div>
+              <div class="b-posted float-left">Posted on: <span><?php echo $article->article_date; ?></span></div>
+              <div class="b-category float-left">Category: <a href="#"><?php echo $article->category_id; ?></a></div>
+              <div class="b-comments float-left"><a href="#">673 comments</a></div>
+              <div class="b-views float-left"><span>2246 views</span></div>
             </div>
             <div class="blog-desc">
-          <?php echo $article->content; ?>
-		  </div>
-           
-          </div>
-		  <?php endforeach; ?>
+          
+          <?php echo $article->article_summary; ?>   </div>
+            <div class="blog-more clearfix">
+              <div class="float-left"><a href="blog-post.html">View details</a></div>
+            </div>
+			
+		
+          </div>  <?php endforeach; ?>
 				 <div class="bottom-navigation clearfix">
             <div class="paginate float-right clearfix">
               <a href="#" class="older"><?php echo $pagination; ?></a>
@@ -30,57 +37,18 @@
 
 				
                
-        <div id="right-side" class="float-left" style="float:right">
+        <div id="right-side" class="float-left" >
           
           <!-- #End // Start blog categories -->
-          <div class="blog-categories">
+           <div class="blog-categories">
             <div class="b-title">Categories</div>
             <ul>
-              <li><a href="#">House / Vila</a></li>
-              <li><a href="#">Studio</a></li>
-              <li><a href="#">1 room apartment</a></li>
-              <li><a href="#">Apartments with 2 bedrooms</a></li>
-              <li class="active"><a href="#">Apartments with 3 bedrooms</a></li>
-              <li><a href="#">Apartments with 4 bedrooms</a></li>
-              <li><a href="#">Apartments with 5+ bedrooms</a></li>
-              <li><a href="#">Lands</a></li>
-              <li><a href="#">Comercial spaces</a></li>
-              <li><a href="#">Office spaces</a></li>
-              <li><a href="#">Industrial spaces</a></li>
-            </ul>
-          </div>
-          <!-- #End // Start popular tags -->
-          <div class="popular-tags clearfix">
-            <div class="b-title">Popular tags</div>
-              <a href="#">design</a>
-              <a href="#">icon</a>
-              <a href="#">grid</a>
-              <a href="#">clean</a>
-              <a href="#">illustration</a>
-              <a href="#">typography</a>
-              <a href="#">pure estate</a>
-              <a href="#">high resolution</a>
-              <a href="#">iphone</a>
-              <a href="#">interface</a>
-              <a href="#">joomla theme</a>
-              <a href="#">portfolio</a>
-              <a href="#">designer</a>
+              <li> <?php foreach($category as $cat): ?> <a href="#">  <?php echo $cat->category_name; ?></a> <?php endforeach; ?></li> </ul>
           </div>
           <!-- #End // Start hot property -->
          
           <!-- #End // Archive -->
-          <div class="archive">
-            <div class="b-title">Archive</div>
-            <ul class="clearfix">
-              <li><a href="#">Octomber<p class="float-right">(43)</p></a></li>
-              <li><a href="#">September<p class="float-right">(91)</p></a></li>
-              <li><a href="#">August<p class="float-right">(6)</p></a></li>
-              <li><a href="#">July<p class="float-right">(24)</p></a></li>
-              <li class="active"><a href="#">June<p class="float-right">(167)</p></a></li>
-              <li><a href="#">May<p class="float-right">(19)</p></a></li>
-              <li><a href="#">April<p class="float-right">(65)</p></a></li>
-            </ul>
-          </div>
+      
           <!-- #End // Stay informed -->
           <div class="b-stay-informed">
             <div class="b-title">Stay informed</div>
@@ -93,4 +61,4 @@
             </div>
           </div>
         </div></div>   
-      </div>
+     </div> 
