6e47298f88ea348851b7bd84e9c451cb8ae2bfe8
kahihia <kajoengasa@yahoo.com>
2013-12-11 17:06:14 +0200

    add blog.css link

--- application/views/header.php
+++ application/views/header.php
@@ -16,6 +16,7 @@
 
     <?php echo HTML::style('css/datepicker.css') ?>
 
+    <?php echo HTML::style('css/blog.css') ?>
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="js/html5shiv.js"></script>
