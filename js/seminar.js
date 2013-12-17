12914396bed014865d02fb5f6dfa79b2bc718ca8
Teamworklab <abhishek@datainfovision.com>
2013-12-17 11:30:47 +0530

    Reverting the changes

--- js/seminar.js
+++ js/seminar.js
@@ -1,7 +1,7 @@
 var baseUrl = '';
 var moduleName = 'conference';
 var baseConfUrl = '';
-var baseProfileUrl = '';
+var baseProfileUrl = ''
 
 $(function(){
 
@@ -10,12 +10,6 @@ $(function(){
 	baseProfileUrl = baseUrl + 'user/profile/';
 
 	$('#attachment-tab a:first').tab('show');
-	
-	$( "#add-video-btn" ).click(opendialog);
-	
-	$( "#add-file-btn" ).click(opendialog2);
-	
-	$( "#add-photo-btn" ).click(opendialog3);
 	
 	$('.book-conf-btn').each(function(index){
 		$(this).on('click', joinSeminar);
@@ -26,6 +20,8 @@ $(function(){
 	});
 
 	$('#add-topic-btn').on('click', addTopic);
+	
+	$( "#add-video-btn" ).click(opendialog);
 
 	$('#back-topic-link').on('click', function(){
 		$('#topic-detail').empty();
@@ -118,183 +114,31 @@ var opendialog = function(e)
 			modal: true,
 			width: 600
 		});
-	var content = 'Add YouTube\'s video';
-	$( ".ui-dialog-title" ).html(content);
 	$( "#add_video" ).click(function( e ) {
 		uploadvideo();
 		e.stopImmediatePropagation();
 	      });
 }
 
-var opendialog2 = function(e)
-{
-	$('#file-upload-container').dialog({
-			modal: true,
-			width: 600
-		});
-	var content = 'Upload attachment for this event';
-	$( ".ui-dialog-title" ).html(content);
-	$( "#add_file" ).click(function( e ) {
-		uploadfile();
-		e.stopImmediatePropagation();
-	      });
-}
-
-var opendialog3 = function(e)
-{
-	$('#photo-upload-container').dialog({
-			modal: true,
-			width: 600
-		});
-	var content = 'Upload photo of this event';
-	$( ".ui-dialog-title" ).html(content);
-	
-	$( "#add_photo" ).click(function( e ) {
-		$("#photo-upload-container").dialog('close');
-		e.stopImmediatePropagation();
-	      });
-}
-
 function uploadvideo() {
 	var confId = $('#conf-id').val();
-	var userId = $('#user-id').val();
 	var url = baseConfUrl + 'upload/' + confId;
 	var data = $('#form-video').serialize()+"&type=video";
 		$("#video-upload-container").dialog('close');
 	
 	$.post(url, data, function(response){
-		if (userId == response.id) {
-			var content = '<div id="'+response.videoid+'"><iframe width="560" height="315" src="http://www.youtube.com/embed/'+response.videoid+'?rel=0" frameborder="0" allowfullscreen></iframe><p class="text-right" onclick="deletevideo(\''+confId+'\',\''+response.videoid+'\');"><a href="#">delete</a></p></div>';
-		}else {
-			var content = '<div id="'+response.videoid+'"><iframe width="560" height="315" src="http://www.youtube.com/embed/'+response.videoid+'?rel=0" frameborder="0" allowfullscreen></iframe>';
-		}
+		var content = '<div id="'+response.videoid+'"><iframe width="560" height="315" src="http://www.youtube.com/embed/'+response.videoid+'?rel=0" frameborder="0" allowfullscreen></iframe><p class="text-right" onclick="deletevideo(\''+confId+'\',\''+response.videoid+'\');"><a href="#">delete</a></p></div>';
 		$("#flex-video").append(content);
-	});
-}
-
-function uploadfile() {
-	var confId = $('#conf-id').val();
-	var url = baseConfUrl + 'uploadfile/' + confId;
-	$("#file-upload-container").dialog('close');
-	
-	/*var fileInput = document.getElementById("filename");
-	
-	var files = fileInput.files;
-	var file;
-	var filename = new Array();
-	var filesize = new Array();
-	var filetype = new Array();
-
-	for (var i = 0; i < files.length; i++) {
-	    file = files.item(i);
-	    file = files[i];
-	
-	    filename[i] = file.name+"|";
-	    filesize[i] = file.size+"|";
-	    filetype[i] = file.type+"|";
-	}
-	
-	var desc = $('#filedesc').val();
-	var data = "filedesc="+desc+"&name="+filename+"&size="+filesize+"&filetype="+filetype+"&type=file";*/
-	/*var data = '';
-	
-	$.post(url, data,function(response){
-		//var content = '<div id="'+response.videoid+'"><iframe width="560" height="315" src="http://www.youtube.com/embed/'+response.videoid+'?rel=0" frameborder="0" allowfullscreen></iframe><p class="text-right" onclick="deletevideo(\''+confId+'\',\''+response.videoid+'\');"><a href="#">delete</a></p></div>';
-		//$("#flex-video").append(content);
-	});*/
-}
-
-function deletefile(filename, confid,deleteid) {
-	var url = baseConfUrl + 'delete/' + confid;
-	var data = "filename="+filename+"&type=file";
-	
-	var r=confirm("Wanna delete this file?");
-	if (r==true){
-		$("#"+deleteid).remove();
-		$.post(url, data, function(response){
-		});
-	}
-}
-
-function deletephoto(photoname, confid,deleteid) {
-	var url = baseConfUrl + 'delete/' + confid;
-	var data = "photoname="+photoname+"&type=photo";
-	
-	var r=confirm("Wanna delete this photo?");
-	if (r==true)
-	{
-		$("#"+deleteid).remove();
-		$.post(url, data, function(response){
-		});
-	}
-}
-
-function editfile(filename, confid, desc, edit_id){
-	$('#filename1').html(filename);
-	$('#filedesc').html(desc);
-	$('#file-edit-container').dialog({
-			modal: true,
-			width: 600
-		});
-	var content = 'Edit file\'s description';
-	$( ".ui-dialog-title" ).html(content);
-	$( "#edit_file" ).click(function( e ) {
-		var newdesc = $('#filedesc').val();
-		edituploadfile(filename, confid, newdesc, edit_id);
-		e.stopImmediatePropagation();
-	      });
-}
-
-function edituploadfile(filename, confid, newdesc, editid){
-	var url = baseConfUrl + 'update/' + confid;
-	var data = "filename="+filename+"&desc="+newdesc+"&type=file";
-	$("#file-edit-container").dialog('close');
-	
-	$.post(url, data, function(response){
-		$('#'+editid).html(newdesc);
-	});
-}
-
-function editphoto(photoname, confid, desc, update_id){
-	$('#photodesc1').val(desc);
-	$('#photo-edit-container').dialog({
-			modal: true,
-			width: 600
-		});
-	var content = 'Edit photo\'s caption';
-	$( ".ui-dialog-title" ).html(content);
-	$('#hiddenval').val(update_id);
-	$('#hiddenname').val(photoname);
-	$( "#edit_photo" ).click(function( e ) {
-		var newdesc = $('#photodesc1').val();
-		$newid = $('#hiddenval').val();
-		$newname = $('#hiddenname').val();
-		edituploadphoto($newname, confid, newdesc, $newid);
-		e.stopImmediatePropagation();
-	      });
-}
-
-function edituploadphoto(photoname, confid, newdesc, updateid){
-	var url = baseConfUrl + 'update/' + confid;
-	var data = "photoname="+photoname+"&desc="+newdesc+"&type=photo";
-	$("#photo-edit-container").dialog('close');
-	
-	$.post(url, data, function(response){
-		$('#'+updateid).attr('title',newdesc);
 	});
 }
 
 function deletevideo(confid,videoid) {
 	var url = baseConfUrl + 'delete/' + confid;
-	var data = "videoid="+videoid+"&type=video";
+	var data = "videoid="+videoid;
 	
-	var r=confirm("Wanna delete this video?");
-	if (r==true)
-	{
-		$("#"+videoid).remove();
-		$.post(url, data, function(response){
-		});
-	}
+	$("#"+videoid).remove();
+	$.post(url, data, function(response){
+	});
 }
 
 var addTopic = function()
